6f3518ac21bf62ea18cd012314d4e39ed52b32c2
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2022-06-30 15:53:56 -0400

    Update mainlayout.blade.php

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FID</title>
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('/favicon.ico) }}">
+
     <link rel="text/javascript" href="{{ asset('/js/bootstrap.min.js') }}" />
 
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" >
