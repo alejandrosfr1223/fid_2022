016df67412ddc859ddb5e831213c1a1a8c2cc370
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2022-06-29 21:20:15 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                         <table id="main" style="text-align: center; align-content: center; margin: auto; ">
                             <tr>
                                 <td>
-                                    <img class="first_img" src="img/logos/logo-fid-llave.png">
+                                    <img class="first_img" src="{{ asset('/img/logos/logo-fid-llave.png') }}">
                                 </td>
                             </tr>
                             <tr>
@@ -23,7 +23,7 @@
                         <table id="notmain" style="text-align: center; align-content: center; margin: auto;">
                             <tr>
                                 <td>
-                                    <img class="first_img" src="img/logos/logo-fid-llave.png">
+                                    <img class="first_img" src="{{ asset('/img/logos/logo-fid-llave.png') }}">
                                 </td>
                                 <td>
                                     <h1 class="first_key">FID</h1>
@@ -42,7 +42,7 @@
         </div>
         <div class="home_container" id="whitebg">
             <div style="position: relative;">
-                <img src="/img/home/logo-overlay2r.png" class="backg-ovrl2 bigscreen ol2-r">
+                <img src="{{ asset('/img/home/logo-overlay2r.png') }}" class="backg-ovrl2 bigscreen ol2-r">
                 <div id='departments_cont'>
                     
                     <div class="leftdivide" id="dep_info_cnt">
@@ -99,11 +99,11 @@
         </div>
     </div>
     <div class="bluebg">
-        <img src="/img/home/logo-overlay2r.png" class="backg-ovrl4 bigscreen ol2-r">
-        <img src="/img/logos/vector-logo.svg" class="backg-ovrl4 smallscreen">
+        <img src="{{ asset('/img/home/logo-overlay2r.png') }}" class="backg-ovrl4 bigscreen ol2-r">
+        <img src="{{ asset('/img/logos/vector-logo.svg') }}" class="backg-ovrl4 smallscreen">
         <div id="div_inapastora">
             <div id="div_inapastoral" class="clmain_divpastora">
-                <img src="/img/juandelrincon/jdr.png" id="divimg" class="changecolorpngsyellow">
+                <img src="{{ asset('/img/juandelrincon/jdr.png') }}" id="divimg" class="changecolorpngsyellow">
             </div>
             <div id="div_inapastorar" class="clmain_divpastora mv_cont2">
                 <h2>{{ trans("home.jdelrincon") }}</h2>
@@ -113,11 +113,11 @@
         </div>
     </div>
     <div class="yellowbg">
-        <img src="/img/home/logo-overlay2.png" class="backg-ovrl3 bigscreen ol2-l">
-        <img src="/img/logos/vector-logo.svg" class="backg-ovrl3 smallscreen">
+        <img src="{{ asset('/img/home/logo-overlay2.png') }}" class="backg-ovrl3 bigscreen ol2-l">
+        <img src="{{ asset('/img/logos/vector-logo.svg') }}" class="backg-ovrl3 smallscreen">
         <div id="div_inapastora">
             <div id="div_inapastoral" class="clmain_divpastora">
-                <img src="/img/home/Divinapastora.png" id="divimg" class="changecolorpngs">
+                <img src="{{ asset('/img/home/Divinapastora.png') }}" id="divimg" class="changecolorpngs">
             </div>
             <div id="div_inapastorar" class="clmain_divpastora mv_cont">
                 <h2>{{ trans("home.divpastora") }}</h2>
@@ -127,9 +127,9 @@
         </div>
     </div>
     <div id="missionvision" class="whitebg" style="position: relative;">
-        <img src="/img/home/logo-overlay2.png" class="backg-ovrl2 bigscreen ol2-l">
-        <img src="/img/home/logo-overlay2r.png" class="backg-ovrl2 bigscreen ol2-r">
-        <img src="/img/logos/vector-logo.svg" class="backg-ovrl2 smallscreen">
+        <img src="{{ asset('/img/home/logo-overlay2.png') }}" class="backg-ovrl2 bigscreen ol2-l">
+        <img src="{{ asset('/img/home/logo-overlay2r.png') }}" class="backg-ovrl2 bigscreen ol2-r">
+        <img src="{{ asset('/img/logos/vector-logo.svg') }}" class="backg-ovrl2 smallscreen">
         <div id="containermv">
             <h2 style="text-align: center; padding-bottom: 20px;">El FID tiene como... </h2>
             <div class="mv_cont" id="mission">
